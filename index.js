b7543530ff7bba1173e1b12b2d0535341519ffc1
Willian <williandeitosi@gmail.com>
2023-10-08 13:30:48 -0300

    teste 6

--- index.js
+++ index.js
@@ -5,6 +5,7 @@ const cors = require("cors");
 
 const app = express();
 app.use(express.json());
+app.use("/", TodoItemRouter);
 
 const PORT = process.env.PORT || 3333;
 
@@ -35,4 +36,3 @@ mongoose
 
 app.listen(PORT, () => console.log(`Server running on port http://${PORT}`));
 
-app.use("/", TodoItemRouter);
